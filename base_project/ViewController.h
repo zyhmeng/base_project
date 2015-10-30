cc7f2ef89488039eb3bcb542b85a5df09856daae
zhangbei <zhangbei@yunfengapp.com>
2015-10-30 10:20:16 +0800

    Initial Commit

---
+++ base_project/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  base_project
+//
+//  Created by jangbuk on 15/10/30.
+//  Copyright © 2015年 jangbuk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ base_project/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  base_project
+//
+//  Created by jangbuk on 15/10/30.
+//  Copyright © 2015年 jangbuk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
